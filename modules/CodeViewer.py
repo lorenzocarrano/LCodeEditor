3d68a6defcaba46a70f03266e71c9c57f5b73257
Lorenzo Carrano <lorenzoc7297@gmail.com>
2022-08-25 01:27:29 +0200

    starting developing syntax highlighting with modularity

--- modules/CodeViewer.py
+++ modules/CodeViewer.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import fileViewer
 import os
 import re
+from syntaxconf import regexList, applyTagCalls, CExtensionsList, PyExtensionsList
 class TextLineNumbers(tk.Canvas):
     def __init__(self, *args, **kwargs):
         tk.Canvas.__init__(self, *args, **kwargs)
@@ -84,53 +85,16 @@ class CodeViewer(tk.Frame):
         #set the right syntax highlighting depending on detected language
         #get the file extension
         fileExtension = self._getFileExtension(self.displayedFile)
-        #get file lines
-        lines = self.text.get(1.0, tk.END).splitlines()
+        #get the index for the syntax highlightinh configuration
+        index = self._syntaxHighlightingConfiguration(fileExtension)
         #prepare a regular expression (language-dependent)
-        regex = re.compile(
-            r"(^\s*"
-            r"(?P<if>if)" + "|"  # if condition
-            r"(?P<for>for)" + "|"  # for loop
-            r"(?P<include>#include\s+[\"<]\S+)" + "|"
-            r"(?P<int>\bint\b)" + "|"   # variable
-            r"(?P<float>\bfloat\b)" + "|" #float variable
-            r"(?P<char>\bchar\b)" + "|" #float variable
-            r"(?P<return>\breturn\b)" +  #return
-            r"[\s\(]+)"
-        )
+        try:
+            regex = regexList[index]
 
-        #apply tags
-        for idx, line in enumerate(lines):
-            int_tag = f"int_{idx}"
-            float_tag = f"float_{idx}"
-            char_tag = f"char_{idx}"
-            for_tag = f"for_{idx}"
-            if_tag = f"if_{idx}"
-            include_tag = f"include_{idx}"
-            return_tag = f"return_{idx}"
-            tags = {
-                int_tag: "blue",
-                float_tag: "blue",
-                char_tag: "blue",
-                for_tag: "green",
-                if_tag: "purple",
-                include_tag: "green",
-                return_tag: "blue"
-                # add new tag here
-            }
-            self._configure_tags(self.text, tags)
-
-            for match in regex.finditer(line):
-                for tag in tags:
-                    group_name = tag.split("_")[0]
-                    if -1 != match.start(group_name):
-                        self.text.tag_add(
-                            tag,
-                            "{0}.{1}".format(idx+1, match.start(group_name)),
-                            "{0}.{1}".format(idx+1, match.end(group_name))
-                        )
-
-        pass
+            #apply tags
+            applyTagCalls[0](self)
+        except:
+            pass
     def _getFileExtension(self, fPath):
         # this will return a tuple of root and extension
         split_tup = os.path.splitext(fPath)
@@ -143,6 +107,14 @@ class CodeViewer(tk.Frame):
         for tag, color in tags.items():
             text_widget.tag_delete(tag)
             text_widget.tag_config(tag, foreground=color)
+
+    def _syntaxHighlightingConfiguration(self, extension):
+        if extension in CExtensionsList:
+            return 0
+        elif extension in PyExtensionsList:
+            return 1
+        else:
+            return 2
 '''
 if __name__ == "__main__":
     root = tk.Tk()
---
+++ modules/syntaxconf.py
@@ -0,0 +1,86 @@
+import re
+import tkinter as tk
+#C Language --> index 0
+CExtensionsList = [".c", ".h", ".cpp", ".hpp", ".inc"]
+Cregex = re.compile(
+        r"(^\s*"
+        r"(?P<if>if)" + "|"  # if condition
+        r"(?P<for>for)" + "|"  # for loop
+        r"(?P<include>#include\s+[\"<]\S+)" + "|"
+        r"(?P<int>\bint\b)" + "|"   # variable
+        r"(?P<float>\bfloat\b)" + "|" #float variable
+        r"(?P<char>\bchar\b)" + "|" #float variable
+        r"(?P<return>\breturn\b)" +  #return
+        r"[\s\(]+)"
+    )
+def applyC_Tags(code):
+    lines = code.text.get(1.0, tk.END).splitlines()
+    for idx, line in enumerate(lines):
+                int_tag = f"int_{idx}"
+                float_tag = f"float_{idx}"
+                char_tag = f"char_{idx}"
+                for_tag = f"for_{idx}"
+                if_tag = f"if_{idx}"
+                include_tag = f"include_{idx}"
+                return_tag = f"return_{idx}"
+                tags = {
+                    int_tag: "blue",
+                    float_tag: "blue",
+                    char_tag: "blue",
+                    for_tag: "green",
+                    if_tag: "purple",
+                    include_tag: "green",
+                    return_tag: "blue"
+                    # add new tag here
+                }
+                code._configure_tags(code.text, tags)
+                for match in Cregex.finditer(line):     #substitute here the proper regex variable!
+                    for tag in tags:
+                        group_name = tag.split("_")[0]
+                        if -1 != match.start(group_name):
+                            code.text.tag_add(
+                                tag,
+                                "{0}.{1}".format(idx+1, match.start(group_name)),
+                                "{0}.{1}".format(idx+1, match.end(group_name))
+                            )
+
+
+#Python Language --> index 1
+PyExtensionsList = [".py"]
+Pyregex = re.compile(
+        r"(^\s*"
+        r"(?P<if>if)" + "|"  # if condition
+        r"(?P<for>for)" + "|"  # for loop
+        r"(?P<import>#import\s+[\"<]\S+)" + "|"
+        r"(?P<return>\breturn\b)" +  #return
+        r"[\s\(]+)"
+    )
+def applyPy_Tags(code):
+    lines = code.text.get(1.0, tk.END).splitlines()
+    for idx, line in enumerate(lines):
+        if_tag = f"if_{idx}"
+        for_tag = f"for_{idx}"
+        import_tag = f"import_{idx}"
+        return_tag = f"return_{idx}"
+        tags = {
+            if_tag: "yellow",
+            for_tag: "yellow",
+            import_tag: "yellow",
+            return_tag: "yellow"
+            # add new tag here
+        }
+        code._configure_tags(code.text, tags)
+        for match in Pyregex.finditer(line):     #substitute here the proper regex variable!
+            for tag in tags:
+                group_name = tag.split("_")[0]
+                if -1 != match.start(group_name):
+                    code.text.tag_add(
+                        tag,
+                        "{0}.{1}".format(idx+1, match.start(group_name)),
+                        "{0}.{1}".format(idx+1, match.end(group_name))
+                    )
+
+
+#lists
+regexList = [Cregex, Pyregex]
+applyTagCalls = [applyC_Tags, applyPy_Tags]
